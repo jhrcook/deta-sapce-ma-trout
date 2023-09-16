58d6f2bb2913b935add9377b9c014893f955839c
Josh Cook <joshuacook0023@gmail.com>
2023-09-16 11:24:41 -0400

    fix: loading of Django secret

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -15,11 +15,10 @@ from typing import Final
 
 from dotenv import load_dotenv
 
-load_dotenv("secrets.py")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / "secrets.txt")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
