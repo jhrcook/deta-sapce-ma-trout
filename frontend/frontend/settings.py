f9befea92d993fded4bc4c5e21bdf616f9121b8a
Joshua Cook <joshua_cook@vrtx.com>
2023-09-17 08:37:32 -0400

    fix merge conflict

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -35,6 +35,7 @@ SECRET_KEY: Final[str] = _get_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS: Final[list[str]] = [
     ".deta.app",
