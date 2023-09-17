2f5d1d8f8274bbc9a41423939f9fee478c865917
Joshua Cook <joshua_cook@vrtx.com>
2023-09-17 08:33:36 -0400

    refactor: use a base HTML and insert table into that

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -3,8 +3,7 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 
-Examples
---------
+Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
@@ -14,13 +13,9 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-
 """
 
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path("dataviz/", include("dataviz.urls")),
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("", include("dataviz.urls")), path("admin/", admin.site.urls)]
