6ff943fb1a7fb52b195ff58fbdd546a898e0f0e6
Joshua Cook <joshua_cook@vrtx.com>
2023-09-21 08:03:05 -0400

    feat: indexing of trout data in a separate Deta Base

--- trout_scraping_server/src/main.rs
+++ trout_scraping_server/src/main.rs
@@ -1,4 +1,4 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 
 use axum::{
     http::StatusCode,
@@ -24,7 +24,7 @@ async fn main() {
     let app = Router::new()
         .route("/", get(root))
         .route("/demo", get(demo))
-        .route("/__space/v0/actions", post(collect_new_data));
+        .route("/__space/v0/actions", post(deta_action_trigger));
     info!("Build app routes.");
 
     let port = match env::var("PORT") {
@@ -49,13 +49,20 @@ async fn root() -> &'static str {
 
 async fn demo() -> impl IntoResponse {
     info!("Running demo data scraping.");
-    let trout_data_organized = get_data().await.organize();
+    let trout_data_organized = get_data().await;
     (StatusCode::OK, Json(trout_data_organized))
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+struct TroutStockingDataMetadata {
+    key: String,
+    timestamp: i64,
+    num_stocked_locations: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct DetaEvent {
-    id: String,
+    id: String, // TODO: refactor this into an Enum
     trigger: String,
 }
 
@@ -64,21 +71,76 @@ struct DetaAction {
     event: DetaEvent,
 }
 
-async fn collect_new_data(Json(payload): Json<DetaAction>) {
+async fn deta_action_trigger(Json(payload): Json<DetaAction>) {
     info!("Recieved payload: {:?}", payload);
+    if payload.event.id == "scrape-new-data" {
+        collect_new_data().await;
+    } else if payload.event.id == "reindex-data" {
+        reindex_trout_stocking_data().await;
+    } else {
+        error!("Unrecognized payload event ID: {}", payload.event.id);
+    }
+}
+
+async fn collect_new_data() {
+    info!("Collecting new trout stocking data.");
     let trout_data = get_data().await;
 
     info!("Creating Deta client.");
     let deta = Deta::new();
 
     // Push raw data.
-    info!("Connecting to Deta Base 'trout-stocking-raw'.");
-    let base = deta.base("trout-stocking-raw");
-    let result = base.insert(trout_data);
+    let db_name = "trout-stocking";
+    info!("Connecting to Deta Base '{db_name}'.");
+    let base = deta.base(db_name);
+    let result = base.insert(&trout_data);
+    let entry_key = match result {
+        Ok(v) => {
+            let key = String::from_str(v["key"].as_str().unwrap()).unwrap();
+            info!("Successfully pushed  data: {}.", key);
+            key
+        }
+        Err(e) => {
+            error!("Error pushing data: {}", e);
+            panic!("Could not push data.")
+        }
+    };
+    info!("data key: {}", entry_key);
+    index_trout_data(&deta, trout_data, entry_key);
+}
+
+async fn reindex_trout_stocking_data() {
+    // TODO: update the "trout-stocking-index" table.
+    info!("Re-indexing trout stocking data.");
+    warn!("Not implemented yet.");
+}
+
+fn index_trout_data(deta: &Deta, trout_data: TroutStocking, key: String) {
+    // Compute metadata values.
+    let timestamp = trout_data.timestamp.timestamp;
+    let num_stocked_locations: u32 = trout_data.organize().data["Species"]
+        .iter()
+        .map(|x| x.is_some() as u32)
+        .sum();
+    let metadata = TroutStockingDataMetadata {
+        key,
+        timestamp,
+        num_stocked_locations,
+    };
+    info!("metadata: {:?}", metadata);
+
+    // Push metadata to index database.
+    let db_name = "trout-stocking-index";
+    info!("Connecting to Deta Base '{db_name}'.");
+    let base = deta.base(db_name);
+    let result = base.insert(metadata);
     match result {
-        Ok(_) => info!("Successfully pushed raw data."),
-        Err(e) => error!("Error pushing raw data: {}", e),
-    }
+        Ok(v) => info!("Successfully pushed metadata: {}.", v),
+        Err(e) => {
+            error!("Error pushing metadata: {}", e);
+            panic!("Could not push metadata.")
+        }
+    };
 }
 
 async fn get_data() -> TroutStocking {
