2eaaf1fe85a747acded6cd8d8faf1e4c8af367d5
Joshua Cook <joshua_cook@vrtx.com>
2023-09-23 10:06:02 -0400

    feat: improvements for using these models in data bases
    - add a key field
    - use alias instead of renaming fields
    - more timestamp features that enable database-side logic

--- trout_scraping/src/models.rs
+++ trout_scraping/src/models.rs
@@ -1,6 +1,7 @@
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DataValue {
@@ -16,7 +17,7 @@ pub struct Row {
 pub struct Column {
     id: String,
     label: String,
-    #[serde(rename(deserialize = "type"))]
+    #[serde(alias = "type")]
     data_type: String,
 }
 
@@ -24,7 +25,7 @@ pub struct Column {
 pub struct QueryTable {
     cols: Vec<Column>,
     rows: Vec<Row>,
-    #[serde(rename(deserialize = "parsedNumHeaders"))]
+    #[serde(alias = "parsedNumHeaders")]
     parsed_num_headers: u32,
 }
 
@@ -56,10 +57,27 @@ impl Default for Timestamp {
     }
 }
 
+impl Timestamp {
+    fn clone(&self) -> Timestamp {
+        Timestamp {
+            datetime: self.datetime.clone(),
+            timestamp: self.timestamp,
+            year: self.year,
+            month: self.month,
+            day: self.day,
+            hour: self.hour,
+            min: self.min,
+            sec: self.sec,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TroutStocking {
+    #[serde(default = "default_key")]
+    pub key: String,
     pub version: String,
-    #[serde(rename(deserialize = "reqId"))]
+    #[serde(alias = "reqId")]
     pub req_id: String,
     pub status: String,
     pub sig: String,
@@ -70,6 +88,7 @@ pub struct TroutStocking {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TroutStockingOrganized {
+    pub key: String,
     pub version: String,
     pub req_id: String,
     pub status: String,
@@ -79,7 +98,7 @@ pub struct TroutStockingOrganized {
 }
 
 impl TroutStocking {
-    pub fn organize(self) -> TroutStockingOrganized {
+    pub fn organize(&self) -> TroutStockingOrganized {
         let mut data = HashMap::new();
         for (c, col) in self.table.cols.iter().enumerate() {
             if !col.label.is_empty() {
@@ -96,12 +115,18 @@ impl TroutStocking {
             }
         }
         TroutStockingOrganized {
-            version: self.version,
-            req_id: self.req_id,
-            status: self.status,
-            sig: self.sig,
+            key: self.key.clone(),
+            version: self.version.clone(),
+            req_id: self.req_id.clone(),
+            status: self.status.clone(),
+            sig: self.sig.clone(),
             data,
-            timestamp: self.timestamp,
+            timestamp: self.timestamp.clone(),
         }
     }
+}
+
+/// Default key from UUID.
+fn default_key() -> String {
+    Uuid::new_v4().to_string()
 }
